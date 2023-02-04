5cdda716af8b29cad93a213cc1fb5e83b77c55da
Michael <mic.bellotti02@gmail.com>
2023-02-04 13:59:57 +0100

    Finito il test del timer con cambio di stato

--- labiroots/js/labiroots/test/test.js
+++ labiroots/js/labiroots/test/test.js
@@ -7,20 +7,20 @@ WL.registerComponent("test", {
         this._myTimer = 15;
     },
     update: function (dt) {
-        var timeLeft = this._myTimer - dt/30;
-        console.log(timeLeft + " | " + _myState);
-        if(this._myState == 3 && timeLeft <= 10){
+        this._myTimer -= dt;
+        //console.log(this._myTimer + " | " + this._myState);
+        if(this._myState == 3 && this._myTimer <= 10){
             console.log(this._myState + " --> " + --this._myState);
-            console.log(timeLeft + "secondi rimanenti");
-        }else if(this._myState == 2 && timeLeft <= 6){
-            console.log(this._myState + " --> " + --this._myFirstTime);
-            console.log(timeLeft + "secondi rimanenti");
-        }else if(this._myState == 1 && timeLeft <= 3){
-            console.log(this._myState + " --> " + --this._myFirstTime);
-            console.log(timeLeft + "secondi rimanenti");
-        }else if(this._myState == 0 && timeLeft <= 0){
-            console.log(this._myState + " --> " + --this._myFirstTime);
-            console.log(timeLeft + "secondi rimanenti");
+            console.log(10 + "secondi rimanenti");
+        }else if(this._myState == 2 && this._myTimer <= 6){
+            console.log(this._myState + " --> " + --this._myState);
+            console.log(6 + "secondi rimanenti");
+        }else if(this._myState == 1 && this._myTimer <= 3){
+            console.log(this._myState + " --> " + --this._myState);
+            console.log(3 + "secondi rimanenti");
+        }else if(this._myState == 0 && this._myTimer <= 0){
+            console.log(this._myState + " --> " + --this._myState);
+            console.log("Sei un albero");
         }
     }
 });
