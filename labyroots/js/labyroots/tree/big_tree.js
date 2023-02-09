ce7b9f1c9aa2adc627d5617b9455e257201f8492
Pipo <eliaducceschi@hotmail.it>
2023-02-09 13:37:00 +0100

    Update leaderbaod on score submitted

--- labyroots/js/labyroots/tree/big_tree.js
+++ labyroots/js/labyroots/tree/big_tree.js
@@ -107,12 +107,12 @@ WL.registerComponent('big-tree', {
                     }
 
                     let score = Math.floor(this._myTimeToWin * 1000);
-                    PP.CAUtils.submitScore("labyroots", score);
-
-                    let leaderboards = WL.scene.pp_getComponents("display-leaderboard");
-                    for (let leaderboard of leaderboards) {
-                        leaderboard.updateLeaderboard();
-                    }
+                    PP.CAUtils.submitScore("labyroots", score, function () {
+                        let leaderboards = WL.scene.pp_getComponents("display-leaderboard");
+                        for (let leaderboard of leaderboards) {
+                            leaderboard.updateLeaderboard();
+                        }
+                    });
                 }
             }
         }
