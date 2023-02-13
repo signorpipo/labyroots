1b73a437721dd287b3f2c57ff7156bff1463dc76
Pipo <eliaducceschi@hotmail.it>
2023-02-13 12:23:42 +0100

    Increase go get her wait

--- labyroots/js/labyroots/cauldron/story.js
+++ labyroots/js/labyroots/cauldron/story.js
@@ -33,7 +33,7 @@ WL.registerComponent('story', {
                     this._myStarted = true;
 
                     if (Global.myIsWeddingTime) {
-                        this._myTimer.start(6);
+                        this._myTimer.start(8);
                     }
                 }
             }
