8b5c4cad3e3082f8a381bd5b82f4e8cc74d9583d
Pipo <eliaducceschi@hotmail.it>
2023-02-14 18:03:11 +0100

    Update main release

--- labyroots/releases/main/labyroots-bundle.js
+++ labyroots/releases/main/labyroots-bundle.js
@@ -40891,6 +40891,8 @@
           WL.onXRSessionEnd.push(this._onXRSessionEnd.bind(this));
           this._myStepDelay = 0.8;
           this._myStepTimer = new PP.Timer(this._myStepDelay);
+          let delay = this._myStepDelay / 5;
+          this._myStepTimer.start(Math.max(0, Math.pp_random(delay - 0.1, delay + 0.05)));
           this._StepRemove = false;
           this._mySteps = [];
           this.stepCounter = 0;
@@ -42355,7 +42357,7 @@
           if (!this._myStarted) {
             if (Global.myStoryReady) {
               if (PP.XRUtils.isSessionActive() || !this._myOnlyVR) {
-                let currentVersion = 7;
+                let currentVersion = 9;
                 console.log("Game Version:", currentVersion);
                 this._myStarted = true;
                 this._myCanSkip = Global.mySaveManager.loadBool("can_skip", false);
@@ -44321,6 +44323,172 @@
     }
   });
 
+  // js/labyroots/cauldron/send_all_events.js
+  var require_send_all_events = __commonJS({
+    "js/labyroots/cauldron/send_all_events.js"() {
+      WL.registerComponent("send-all-events", {}, {
+        init: function() {
+        },
+        start: function() {
+          this._myStarted = false;
+        },
+        update: function(dt) {
+          if (!this._myStarted) {
+            if (Global.myReady) {
+              this._myStarted = true;
+              if (Global.myGoogleAnalytics) {
+                gtag("event", "button_pressed", {
+                  "value": 1
+                });
+                gtag("event", "enter_vr", {
+                  "value": 1
+                });
+                gtag("event", "open_ggj_success", {
+                  "value": 1
+                });
+                gtag("event", "open_ggj", {
+                  "value": 1
+                });
+                gtag("event", "open_github_success", {
+                  "value": 1
+                });
+                gtag("event", "open_github", {
+                  "value": 1
+                });
+                gtag("event", "open_zesty", {
+                  "value": 1
+                });
+                gtag("event", "open_zesty_success", {
+                  "value": 1
+                });
+                gtag("event", "enter_secret_zone", {
+                  "value": 1
+                });
+                gtag("event", "zesty_load_fail", {
+                  "value": 1
+                });
+                gtag("event", "intro_skipped", {
+                  "value": 1
+                });
+                gtag("event", "intro_watched", {
+                  "value": 1
+                });
+                gtag("event", "playing_signed_in", {
+                  "value": 1
+                });
+                gtag("event", "is_wedding_maze", {
+                  "value": 1
+                });
+                gtag("event", "is_normal_maze", {
+                  "value": 1
+                });
+                gtag("event", "collect_axe", {
+                  "value": 1
+                });
+                gtag("event", "collect_axe_after_death", {
+                  "value": 1
+                });
+                gtag("event", "collect_axe_before_death", {
+                  "value": 1
+                });
+                gtag("event", "secret_code_wedding_success", {
+                  "value": 1
+                });
+                gtag("event", "secret_code_human_tree_success", {
+                  "value": 1
+                });
+                gtag("event", "secret_code_human_tree", {
+                  "value": 1
+                });
+                gtag("event", "secret_code_wedding", {
+                  "value": 1
+                });
+                gtag("event", "death", {
+                  "value": 1
+                });
+                gtag("event", "survive_for_seconds", {
+                  "value": 1
+                });
+                gtag("event", "survive_bear_grills", {
+                  "value": 1
+                });
+                gtag("event", "survive_a_lot", {
+                  "value": 1
+                });
+                gtag("event", "survive_more", {
+                  "value": 1
+                });
+                gtag("event", "defeat_mother_tree", {
+                  "value": 1
+                });
+                gtag("event", "defeat_mother_tree_seconds", {
+                  "value": 1
+                });
+                gtag("event", "collect_fruit", {
+                  "value": 1
+                });
+                gtag("event", "eat_fruit", {
+                  "value": 1
+                });
+                gtag("event", "eat_fruit_perfect", {
+                  "value": 1
+                });
+                gtag("event", "eat_fruit_good", {
+                  "value": 1
+                });
+                gtag("event", "eat_fruit_evil", {
+                  "value": 1
+                });
+                gtag("event", "eat_fruit_bad", {
+                  "value": 1
+                });
+                gtag("event", "defeat_human_tree", {
+                  "value": 1
+                });
+                gtag("event", "defeat_bride_tree", {
+                  "value": 1
+                });
+                gtag("event", "defeat_root_wall", {
+                  "value": 1
+                });
+                gtag("event", "defeat_root", {
+                  "value": 1
+                });
+                gtag("event", "switch_teleport", {
+                  "value": 1
+                });
+                gtag("event", "switch_smooth", {
+                  "value": 1
+                });
+                gtag("event", "moving_non_vr", {
+                  "value": 1
+                });
+                gtag("event", "html5_audio_error_reload", {
+                  "value": 1
+                });
+                let timeMovingSteps = [1, 3, 5, 10, 20, 30, 60];
+                for (let timeMovingStep of timeMovingSteps) {
+                  gtag("event", "moving_for_" + timeMovingStep + "_minutes_vr", {
+                    "value": 1
+                  });
+                  gtag("event", "moving_for_" + timeMovingStep + "_minutes_non_vr", {
+                    "value": 1
+                  });
+                }
+                let timeGrabbedSteps = [5, 10, 15, 30];
+                for (let timeGrabbedStep of timeGrabbedSteps) {
+                  gtag("event", "fruit_grab_for_" + timeGrabbedStep + "_seconds", {
+                    "value": 1
+                  });
+                }
+              }
+            }
+          }
+        }
+      });
+    }
+  });
+
   // js/labyroots/player/transformation.js
   var require_transformation = __commonJS({
     "js/labyroots/player/transformation.js"() {
@@ -45961,6 +46129,7 @@
   require_show_drifting();
   require_increase_floor_tile();
   require_render_closest_walls_first();
+  require_send_all_events();
   require_transformation();
   require_stage_switch();
   require_mouth();
