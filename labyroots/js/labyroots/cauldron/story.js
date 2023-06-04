53f0a73e8c58072bf69186ae2c7a86476cb3caec
Pipo <eliaducceschi@hotmail.it>
2023-06-04 19:06:07 +0200

    Increase game version

--- labyroots/js/labyroots/cauldron/story.js
+++ labyroots/js/labyroots/cauldron/story.js
@@ -28,7 +28,7 @@ WL.registerComponent('story', {
         if (!this._myStarted) {
             if (Global.myStoryReady) {
                 if (PP.XRUtils.isSessionActive() || !this._myOnlyVR) {
-                    let currentVersion = 20;
+                    let currentVersion = 21;
                     console.log("Game Version:", currentVersion);
 
                     this._myStarted = true;
