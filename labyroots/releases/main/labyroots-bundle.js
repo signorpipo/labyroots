429a89b99ba3e04c6bc63eec2f5b48043bd9bf4b
Pipo <eliaducceschi@hotmail.it>
2023-06-06 18:25:04 +0200

    Update main release

--- labyroots/releases/main/labyroots-bundle.js
+++ labyroots/releases/main/labyroots-bundle.js
@@ -43601,7 +43601,7 @@
           if (!this._myStarted) {
             if (Global.myStoryReady) {
               if (PP.XRUtils.isSessionActive() || !this._myOnlyVR) {
-                let currentVersion = 21;
+                let currentVersion = 22;
                 console.log("Game Version:", currentVersion);
                 this._myStarted = true;
                 this._myCanSkip = Global.mySaveManager.loadBool("can_skip", false);
