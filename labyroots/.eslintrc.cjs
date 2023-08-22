24b8dd52e0694ff349af77df636bdae7e6b46715
Pipo <eliaducceschi@hotmail.it>
2023-08-22 19:53:52 +0200

    Add eslint

---
+++ labyroots/.eslintrc.cjs
@@ -0,0 +1,51 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        project: "tsconfig.json",
+        tsconfigRootDir: __dirname
+    },
+    env: {
+        browser: true,
+        node: true
+    },
+    globals: {
+        "WL": "readonly",
+        "PP": "readonly",
+        "LR": "writable",
+        "Global": "writable",
+        "AudioID": "writable",
+        "downloadFileText": "writable",
+        "downloadFileJSON": "writable",
+        "loadFileText": "writable",
+        "loadFileJSON": "writable",
+        "loadFile": "writable"
+    },
+    plugins: [
+        "deprecation"
+    ],
+    extends: [
+        "eslint:recommended"
+    ],
+    rules: {
+        "no-unused-vars": ["warn", { "args": "none", "varsIgnorePattern": "^__" }],
+        "deprecation/deprecation": "warn"
+    },
+    ignorePatterns: [
+        "/node_modules/",
+        "/deploy/",
+        "/cache/",
+        "/.editor/",
+        "/languages/",
+        "/.editor/",
+        "/assets/",
+        "/static/",
+        ".eslintrc.cjs",
+        "zesty.js",
+        "zesty-wonderland-sdk-compat.js"
+    ],
+    overrides: [
+    ]
+};
