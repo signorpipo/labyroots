584c49ae01486fed4c048153f6409092bb358cfb
Elia <eliaducceschi@hotmail.it>
2023-10-28 09:12:04 +0200

    Ca Utils refactor progress

--- labyroots/js/labyroots/cauldron/lr_ca_dummy_server.js
+++ labyroots/js/labyroots/cauldron/lr_ca_dummy_server.js
@@ -3,24 +3,39 @@ LR.LRCADummyServer = class LRCADummyServer {
     constructor() {
     }
 
-    getLeaderboard(leaderboardID, isAscending, isAroundPlayer, scoresAmount, callbackOnDone, callbackOnError) {
+    getLeaderboard(leaderboardID, ascending, aroundPlayer, scoresAmount, onDoneCallback = null, onErrorCallback = null, caError = PP.CAUtils.CAError.NONE) {
         let leaderboard = null;
 
-        if (PP.CAUtils.isSDKAvailable()) {
-            leaderboard = [
-                { rank: 0, displayName: "An", score: 0 },
-                { rank: 1, displayName: "Error", score: 0 },
-                { rank: 2, displayName: "Has", score: 0 },
-                { rank: 3, displayName: "Occurred", score: 0 },
-                { rank: 4, displayName: "While", score: 0 },
-                { rank: 5, displayName: "Trying", score: 0 },
-                { rank: 6, displayName: "To", score: 0 },
-                { rank: 7, displayName: "Retrieve", score: 0 },
-                { rank: 8, displayName: "The", score: 0 },
-                { rank: 9, displayName: "Leaderboard", score: 0 }
-            ];
+        if (caError != PP.CAUtils.CAError.CA_SDK_MISSING) {
+            if (aroundPlayer && (caError == PP.CAUtils.CAError.USER_NOT_LOGGED_IN || caError == PP.CAUtils.CAError.USER_HAS_NO_SCORE)) {
+                leaderboard = [
+                    { rank: 0, displayName: "Sign In", score: 0 },
+                    { rank: 1, displayName: "And", score: 0 },
+                    { rank: 2, displayName: "Play", score: 0 },
+                    { rank: 3, displayName: "On", score: 0 },
+                    { rank: 4, displayName: "HeyVR", score: 0 },
+                    { rank: 5, displayName: "To", score: 0 },
+                    { rank: 6, displayName: "Submit", score: 0 },
+                    { rank: 7, displayName: "Your", score: 0 },
+                    { rank: 8, displayName: "Own", score: 0 },
+                    { rank: 9, displayName: "Score", score: 0 }
+                ];
+            } else {
+                leaderboard = [
+                    { rank: 0, displayName: "An", score: 0 },
+                    { rank: 1, displayName: "Error", score: 0 },
+                    { rank: 2, displayName: "Has", score: 0 },
+                    { rank: 3, displayName: "Occurred", score: 0 },
+                    { rank: 4, displayName: "While", score: 0 },
+                    { rank: 5, displayName: "Trying", score: 0 },
+                    { rank: 6, displayName: "To", score: 0 },
+                    { rank: 7, displayName: "Retrieve", score: 0 },
+                    { rank: 8, displayName: "The", score: 0 },
+                    { rank: 9, displayName: "Leaderboard", score: 0 }
+                ];
+            }
         } else {
-            if (isAroundPlayer) {
+            if (aroundPlayer) {
                 leaderboard = [
                     { rank: 0, displayName: "Sign In", score: 0 },
                     { rank: 1, displayName: "And", score: 0 },
@@ -53,23 +68,23 @@ LR.LRCADummyServer = class LRCADummyServer {
             leaderboard.pop();
         }
 
-        if (callbackOnDone) {
-            callbackOnDone(leaderboard);
+        if (onDoneCallback != null) {
+            onDoneCallback(leaderboard);
         }
     }
 
-    submitScore(leaderboardID, scoreToSubmit, callbackOnDone, callbackOnError) {
-        if (callbackOnDone) {
-            callbackOnDone();
+    submitScore(leaderboardID, scoreToSubmit, onDoneCallback = null, onErrorCallback = null, caError = PP.CAUtils.CAError.NONE) {
+        if (onDoneCallback != null) {
+            onDoneCallback();
         }
     }
 
-    getUser(callbackOnDone, callbackOnError) {
+    getUser(onDoneCallback = null, onErrorCallback = null, caError = PP.CAUtils.CAError.NONE) {
         let user = {};
         user.displayName = "I Polaretti";
 
-        if (callbackOnDone) {
-            callbackOnDone(user);
+        if (onDoneCallback != null) {
+            onDoneCallback(user);
         }
     }
 };
