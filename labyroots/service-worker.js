564ea1fc9e0e995c51fbc38f6c756c900a8ec77e
Elia <eliaducceschi@hotmail.it>
2024-02-08 15:46:42 +0100

    Update links and remove forEach

--- labyroots/service-worker.js
+++ labyroots/service-worker.js
@@ -65,7 +65,7 @@ let _myCacheVersion = 7;
 // In general, u should precache at least every static resource u have in your app if u want to make it work offline after the first load
 //
 // The resources URLs can be relative to the service worker location, so, for example,
-// for "https://signor-pipo.itch.io/assets/wondermelon.png" u can just specify "assets/wondermelon.png"
+// for "https://signorpipo.itch.io/assets/wondermelon.png" u can just specify "assets/wondermelon.png"
 // The resources URLs can't be a regex in this case, since it needs to know the specific resource to fetch
 let _myResourceURLsToPrecache = [
     "/",
@@ -258,8 +258,8 @@ let _myForceTryFetchFromCacheFirstOnNetworkErrorResourceURLsToExclude = _NO_RESO
 //
 // Sometimes though this could be used for other reasons, for example they can be used on the index URL to give parameters to the app,
 // and not really to fetch a different resource
-// Like doing "https://signor-pipo.itch.io/?useWondermelon=true" to specify that a certain feature should be turned on
-// If only "https://signor-pipo.itch.io/" is cached, when u ask for the above URL, the cache will fail
+// Like doing "https://signorpipo.itch.io/?useWondermelon=true" to specify that a certain feature should be turned on
+// If only "https://signorpipo.itch.io" is cached, when u ask for the above URL, the cache will fail
 // U can turn on this feature to ignore the URL params when checking the cache
 // In this specific case u can use @_IGNORE_INDEX_URL_PARAMS to specify that only the index URL should be allowed to ignore them
 //
@@ -653,7 +653,7 @@ let _myImmediatelyTakeControlOfAllPages = false;
 // u can decide if u want to reload even pages that were not previously controlled by any service worker or not
 //
 // This is due to the fact that @_myReloadAllPagesOnServiceWorkerActivation can only reload pages already controlled, while
-// this flag let you also reload the pages that will be controlled thanks to @ _myImmediatelyTakeControlOfAllPages
+// this flag let you also reload the pages that will be controlled thanks to @_myImmediatelyTakeControlOfAllPages
 let _myReloadAllPagesAfterImmediatelyTakingControlOfThem = false;
 
 
@@ -878,7 +878,9 @@ async function _activate() {
 
         if (_myReloadAllPagesOnServiceWorkerActivation) {
             let clients = await self.clients.matchAll();
-            clients.forEach(client => client.navigate(client.url));
+            for (let client of clients) {
+                client.navigate(client.url);
+            }
         }
 
         if (_myImmediatelyTakeControlOfAllPages) {
@@ -886,7 +888,9 @@ async function _activate() {
 
             if (_myReloadAllPagesAfterImmediatelyTakingControlOfThem) {
                 let clients = await self.clients.matchAll();
-                clients.forEach(client => client.navigate(client.url));
+                for (let client of clients) {
+                    client.navigate(client.url);
+                }
             }
         }
     } catch (error) {
@@ -906,7 +910,9 @@ async function _activate() {
 
             let clients = await self.clients.matchAll();
             await self.registration.unregister();
-            clients.forEach(client => client.navigate(client.url));
+            for (let client of clients) {
+                client.navigate(client.url);
+            }
 
             let logEnabled = _shouldResourceURLBeIncluded(_getCurrentLocation(), _myLogEnabledLocationURLsToInclude, _myLogEnabledLocationURLsToExclude);
             if (logEnabled) {
